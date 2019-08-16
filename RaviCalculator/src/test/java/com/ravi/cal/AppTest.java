4b29f2ab1f5eee42aa03e79698fab262438986f4
kotha kalyan <kalyankumar.kotha@gmail.com>
2019-08-16 06:11:05 +0000

    test cases

--- RaviCalculator/src/test/java/com/ravi/cal/AppTest.java
+++ RaviCalculator/src/test/java/com/ravi/cal/AppTest.java
@@ -5,8 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AppTest 
-    extends TestCase
-{
+    {
    @Test
     public void twoAndThreeIsFive() throws Exception {
         final long result = new Calculator().addFucn(2, 3);
